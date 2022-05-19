872152f2afac1101976a0b8676020efe3a2144ee
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2022-05-19 09:19:17 -0300

    Initial commit

---
+++ src/test/java/com/leords/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leords.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
