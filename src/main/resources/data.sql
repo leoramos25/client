4eb21f7da66aed7b954ccd16c36915d284f0a9e2
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2022-05-19 10:15:09 -0300

    Client entity, h2 configuration and database seeding

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Gabriel', '12345678901', 3000.00, TIMESTAMP WITH TIME ZONE '1999-08-25T23:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Joelma', '09876543210', 2000.00,  TIMESTAMP WITH TIME ZONE '1973-09-25T07:30:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Geraldo', '12345678901', 8000.00,  TIMESTAMP WITH TIME ZONE '1965-10-16T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Leonardo', '47556699904', 2400.00, TIMESTAMP WITH TIME ZONE '1999-05-25T08:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Wagner', '12398765437', 7500.00, TIMESTAMP WITH TIME ZONE '1999-05-28T20:00:00Z', 1);
